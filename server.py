d542370ae43445e634f43b63282ecc35c385152d
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-12-04 20:27:11 -0300

    feat: two-way communication

--- server.py
+++ server.py
@@ -6,7 +6,11 @@ UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-
+mandou = False
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message: %s" % data)
+    MESSAGE = b'recebido!'
+    if mandou == False:
+        sock.sendto(MESSAGE, addr)
+        mandou = True
