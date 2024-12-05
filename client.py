875cce2f341736f06f4a2d09e3e4012c6da20e3a
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2024-12-05 00:12:40 +0000

    :sparkles: feat: funcionamento das pétalas implementado

--- client.py
+++ client.py
@@ -10,9 +10,22 @@ print("message: %s" % MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
+MESSAGE = str.encode(input("Qual a quantidade de pétalas? "))
 #sock.bind((UDP_IP, UDP_PORT))
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 
+data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+print("received message: %s" % data)
+    
 while True:
+    MESSAGE = str.encode(input("Retire a pétala!"))
+    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print("received message: %s" % data)
+    
+    data = data.decode('utf-8')
+
+    if (data == "Ele(a) te ama!") or (data == "Ele(a) não te ama!"):
+        break
+    
--- server.py
+++ server.py
@@ -7,10 +7,30 @@ sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 mandou = False
+bem_me_quer = False
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message: %s" % data)
+    petalas_qtd = int(data)
+    print("Quantidade de Petalas: %s" % petalas_qtd)
     MESSAGE = b'recebido!'
     if mandou == False:
         sock.sendto(MESSAGE, addr)
         mandou = True
+    while petalas_qtd > 0:
+        data, addr = sock.recvfrom(1024)
+        petalas_qtd -= 1 
+        mensagem = str(petalas_qtd)
+        if petalas_qtd % 2 == 0:
+            mensagem = "Bem me Quer"
+            bem_me_quer = True
+        if petalas_qtd % 2 == 1:
+            mensagem = "Mal me Quer"
+            bem_me_quer = False
+        sock.sendto(str.encode(mensagem), addr)
+    
+    if bem_me_quer == True:
+        mensagem = "Ele(a) te ama!"
+    else:
+        mensagem = "Ele(a) não te ama!"
+    sock.sendto(str.encode(mensagem), addr)
+    break
